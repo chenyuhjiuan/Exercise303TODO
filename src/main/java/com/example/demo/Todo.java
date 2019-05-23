48753caa700a79f54c31e7aa85c97a59dd480ef0
chenyuhjiuan <emilymarinovich@hotmail.com>
2019-05-23 14:21:06 -0400

    Complete Data Lifecycle Excercise303

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotNull
+    @Size(min = 3)
+    private String title;
+
+    @NotNull
+    @Size(min = 8, max=8)
+    private String duedate;
+
+    @NotNull
+    @Min(1)
+    private int priority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDuedate() {
+        return duedate;
+    }
+
+    public void setDuedate(String duedate) {
+        this.duedate = duedate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
